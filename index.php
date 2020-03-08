eb6a5fd989a9516fcd972e5d46d1139a313fdeab
VitaliyBulyma <vitaliy_bulyma@yahoo.ca>
2020-03-08 10:43:52 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Phph Calculator</title>
+</head>
+<body>
+
+ <form action=""></form>
+    <input type="text" name="num1" id="num1" placeholder="Number 1">
+    <input type="text" name="num2" id="num2" placeholder="Number 2">
+</body>
+</html>
